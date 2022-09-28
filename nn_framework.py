c594155bbefb7b326aa225634ab42d9b5f86afce
Diebrate <kevinkw.zhang@mail.utoronto.ca>
2022-09-28 16:21:39 -0400

    update

--- nn_framework.py
+++ nn_framework.py
@@ -1651,13 +1651,13 @@ class NeuralNetwork2D(nn.Module):
 
     def forward(self, x):
         return self.flow(x.float())
-    
-    
+
+
 class NeuralVol2D(nn.Module):
-    
+
     def __init__(self, d_in, d_out, d_hid, n_layers=20):
-        super().__init__():
-            layers = []
+        super().__init__()
+        layers = []
         layers.append(nn.Conv2d(d_in, d_hid, kernel_size=1, padding='same'))
         layers.append(nn.ReLU())
         layers.append(nn.BatchNorm2d(d_hid))
