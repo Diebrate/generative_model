e948aae30d10e1b78f3ef5578095b0e9d0f8085c
Diebrate <kevinkw.zhang@mail.utoronto.ca>
2022-09-30 09:44:10 -0400

    update

--- check.py
+++ check.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import torch
 from torch import nn
 
@@ -11,51 +14,67 @@ start = time.time()
 
 np.random.seed(111)
 
-coeff = np.ones(3) / 3
+reg = 10
+reg_phi = 50
 
-p0 = np.array([0.1, 0.2, 0.3, 0.4])
-p1 = np.array([0.25, 0.25, 0.25, 0.25])
-p2 = np.array([0.4, 0.4, 0.1, 0.1])
+N = 1000
 
-costm = np.random.uniform(size=(4, 4))
-costm = costm + costm.T
-np.fill_diagonal(costm, 0)
+x1 = np.random.multivariate_normal(mean=[-10, 0], cov=[[1, 0], [0, 1]], size=N)
+x2 = np.random.multivariate_normal(mean=[10, 0], cov=[[1, 0], [0, 1]], size=N)
 
-reg = 100
-reg_phi = 1
+gen = nn_framework.NeuralNetwork(4, 2, 100, n_layers=4)
+opt_gen = torch.optim.Adam(gen.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
 
-### optimal transport
+err_ent = nn_framework.NeuralNetwork(3, 1, 100, n_layers=4)
+opt_ent = torch.optim.Adam(err_ent.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
 
-# balanced (approx)
-mm_ot_eq = ot_num.multimarg_unbalanced_ot(p0, p1, p2, costm=costm, reg=reg, reg_phi=10000, coeff=coeff, n_iter=1000)
+err_kl = nn_framework.NeuralNetwork(3, 1, 100, n_layers=4)
+opt_kl = torch.optim.Adam(err_kl.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
 
-# unbalanced
-mm_ot_neq = ot_num.multimarg_unbalanced_ot(p0, p1, p2, costm=costm, reg=reg, reg_phi=reg_phi, coeff=coeff, n_iter=1000)
+m = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(2),
+                                                               torch.tensor(np.diag([1, 1])).float())
+obj = []
 
-### neural network
+t_step = 50
+dt = 1 / t_step
+t_list = np.linspace(0, 1, t_step + 1)
 
-softmax = nn.Softmax(dim=1)
-
-# balanced
-mod_eq = nn_framework.NeuralNetwork(3 * 4, 4, 100, n_layers=16)
-opt_eq = torch.optim.Adam(mod_eq.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
-obj_eq = []
-for n in range(1000):
-    x_sim = torch.zeros((100, 3))
-    x_sim[:, 0] = torch.multinomial(torch.tensor(p0), 100, replacement=True)
-    x_sim[:, 1] = torch.multinomial(torch.tensor(p1), 100, replacement=True)
-    x_sim[:, 2] = torch.multinomial(torch.tensor(p2), 100, replacement=True)
-    inp = torch.zeros((100, 3, 4))
-    for i in range(100):
-        inp[i, torch.arange(3).long(), x_sim[i].long()] = 1
-    output = softmax(mod_eq(inp))
-    l = torch.einsum('bij,jk->bik', inp.double(), torch.tensor(costm))
-    l = torch.einsum('bij,bj->bi', l, output.double())
-    l = l.sum(axis=1).mean()
-    l = l + output
-    opt_eq.zero_grad()
+for n in range(500):
+    l = torch.tensor(0.)
+    x1_temp = torch.tensor(x1[np.random.choice(np.arange(N), replace=True, size=100)])
+    x2_temp = torch.tensor(x2[np.random.choice(np.arange(N), replace=True, size=100)])
+    for it in range(t_step):
+        inp1 = torch.cat([x1_temp, t_list[it] * torch.ones(100, 1)], dim=1)
+        inp2 = torch.cat([x2_temp, t_list[it] * torch.ones(100, 1)], dim=1)
+        out1 = nn.functional.relu(err_kl(inp1))
+        out2 = nn.functional.relu(err_kl(inp2))
+        x1_temp = x1_temp + out1 * m.sample([100]) * np.sqrt(dt)
+        x2_temp = x2_temp + out2 * m.sample([100]) * np.sqrt(dt)
+        l = l + 0.5 * reg_phi * out1.pow(2).mean() * dt + 0.5 * reg_phi * out2.pow(2).mean() * dt
+    x = torch.cat([x1_temp, x2_temp], dim=1)
+    y = gen(x)
+    for it in range(t_step):
+        inp = torch.cat([y, t_list[it] * torch.ones(100, 1)], dim=1)
+        out = nn.functional.relu(err_ent(inp))
+        y = y + out * m.sample([100]) * np.sqrt(dt)
+        l = l - reg * out.pow(2).mean() * dt
+    l = l + (x1_temp - y).pow(2).sum(axis=1).mean()
+    l = l + (x2_temp - y).pow(2).sum(axis=1).mean()
+    opt_kl.zero_grad()
+    opt_gen.zero_grad()
+    opt_ent.zero_grad()
     l.backward()
-    opt_eq.step()
-    obj_eq.append(float(l))
+    opt_kl.step()
+    opt_gen.step()
+    opt_ent.step()
+    obj.append(float(l))
+    print('obj = {0:0.5f} at iteration {1:n}'.format(float(obj[-1]), n))
+
+df = np.vstack((x1[np.random.choice(np.arange(N), replace=True, size=100)],
+                x2[np.random.choice(np.arange(N), replace=True, size=100)],
+                y.detach().numpy()))
+df = pd.DataFrame(df, columns=['x', 'y'])
+df['group'] = np.repeat(['s1', 's2', 'b'], 100)
+sns.scatterplot(x='x', y='y', data=df, hue='group', linewidth=0, s=3)
 
 print('-----process takes {:0.6f} seconds-----'.format(time.time() - start))
--- image.py
+++ image.py
@@ -10,38 +10,20 @@ import torchvision.transforms as transforms
 
 import nn_framework
 
-data = 'MNIST'
+data = 'CelebA'
 path = 'data/' + data
 load = getattr(torchvision.datasets, data)
 
-transform = transforms.Compose([transforms.ToTensor()])
-# transform = transforms.Compose([transforms.ToTensor(),
-#                                 transforms.Normalize((0., 0., 0.), (1., 1., 1.))])
+# transform = transforms.Compose([transforms.ToTensor()])
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0., 0., 0.), (1., 1., 1.))])
 
 if not os.path.exists(path):
     os.mkdir(path)
 
 if len(os.listdir(path)) > 0:
-    data = load(path, download=False, train=True, transform=transform)
+    data = load(path, download=False, transform=transform)
 else:
-    data = load(path, download=True, train=True, transform=transform)
+    data = load(path, download=True, transform=transform)
 
-# dataloader = torch.utils.data.DataLoader(data)
-
-x = torch.stack([i[0] for i in data])
-labels = torch.tensor([i[1] for i in data])
-label_list = data.classes
-
-pick = 6
-z = x[labels == 6]
-
-gen = nn_framework.NeuralNetwork2D(1, 1, 10, n_layers=50)
-
-n_iter = 1000
-n_sample = 100
-
-l = torch.tensor(0)
-for n in range(n_iter):
-    ind = np.random.choice(np.arange(z.shape[0]), size=n_sample)
-    z0 = z[ind]
-    # cost matrix
+dataloader = torch.utils.data.DataLoader(data)
