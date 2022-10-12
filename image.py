48aaddae3e2d3773e9c4ea301fe9d0c39570ee11
Diebrate <kevinkw.zhang@mail.utoronto.ca>
2022-10-12 06:29:36 -0400

    update

--- image.py
+++ image.py
@@ -48,18 +48,18 @@ for ind, samp in enumerate(dataloader_sub):
     # plt.imshow(samp[0][0].numpy().transpose((1, 2, 0)))
 
 df_sub = torch.cat(df_sub)
-plt.imshow(torchvision.utils.make_grid(df_sub).numpy().transpose(1, 2, 0))
+# plt.imshow(torchvision.utils.make_grid(df_sub).numpy().transpose(1, 2, 0))
 # plt.savefig('image/true_' + str(person_id) + '.png')
 
 d_max = df_sub.shape[0]
 
 d = d_max
 
-n_iter = 250
+n_iter = 300
 
 reg = 1e-5
-reg_phi = 1e-2
-n_layers = 4
+reg_phi = 1e-5
+n_layers = 2
 d_hid = 3
 
 obj = []
@@ -80,15 +80,19 @@ if comp_joint:
 
     err_kl = nn_framework.NeuralNetwork2D(3 * d, 3 * d, d_hid, n_layers=n_layers)
 
-    opt = torch.optim.Adam(list(gen.parameters()) + list(err_ent.parameters()) + list(err_kl.parameters()), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
+    param = list(gen.parameters()) + list(err_ent.parameters()) + list(err_kl.parameters())
+
+    opt = torch.optim.Adam(param, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
 
     for n in range(n_iter):
         l = torch.tensor(0.)
         if d == d_max:
-            joint = df_sub.flatten(start_dim=0, end_dim=1)
+            joint_raw = df_sub.flatten(start_dim=0, end_dim=1)
         else:
-            joint = df_sub[np.random.choice(np.arange(d_max), replace=True, size=d)].flatten(start_dim=0, end_dim=1)
-        joint = joint.unsqueeze(0)
+            joint_raw = df_sub[np.random.choice(np.arange(d_max), replace=True, size=d)].flatten(start_dim=0, end_dim=1)
+        joint_raw = joint_raw.unsqueeze(0)
+        joint_raw = torch.clamp(joint_raw, min=0.0001, max=0.9999)
+        joint = torch.logit(joint_raw)
         for it in range(t_step):
             out_kl = err_kl(joint)
             joint = joint + out_kl * dt
@@ -98,7 +102,8 @@ if comp_joint:
             out = reg * torch.sigmoid(err_ent(z))
             z = z + out * m.sample(z.shape) * np.sqrt(dt)
             l = l - reg * out.pow(2).mean() * dt
-        l = l + (joint - torch.tile(z, dims=[1, d, 1, 1])).pow(2).mean()
+        z = torch.sigmoid(z)
+        l = l + (joint_raw - torch.tile(z, dims=[1, d, 1, 1])).pow(2).mean()
         opt.zero_grad()
         l.backward()
         opt.step()
@@ -113,29 +118,35 @@ else:
 
     err_kl = nn_framework.NeuralNetwork2D(3 * d, 3 * d, d_hid, n_layers=n_layers)
 
-    opt = torch.optim.Adam(list(gen.parameters()) + list(err_ent.parameters()) + list(err_kl.parameters()), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
+    param = list(gen.parameters()) + list(err_ent.parameters()) + list(err_kl.parameters())
+
+    opt = torch.optim.Adam(param, lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=5e-5)
 
     m_noise = torch.distributions.uniform.Uniform(0, 1)
     noise_dim = torch.tensor(df_sub.shape)
     noise_dim[0] = 1
+
     for n in range(n_iter):
         l = torch.tensor(0.)
         if d == d_max:
-            joint = df_sub.flatten(start_dim=0, end_dim=1)
+            joint_raw = df_sub.flatten(start_dim=0, end_dim=1)
         else:
-            joint = df_sub[np.random.choice(np.arange(d_max), replace=True, size=d)].flatten(start_dim=0, end_dim=1)
-        joint = joint.unsqueeze(0)
+            joint_raw = df_sub[np.random.choice(np.arange(d_max), replace=True, size=d)].flatten(start_dim=0, end_dim=1)
+        joint_raw = joint_raw.unsqueeze(0)
+        joint_raw = torch.clamp(joint_raw, min=0.0001, max=0.9999)
+        joint = torch.logit(joint_raw)
         for it in range(t_step):
             out_kl = err_kl(joint)
             joint = joint + out_kl * dt
             l = l + reg_phi * out_kl.pow(2).mean() * dt
-        noise = m_noise.sample(noise_dim)
-        z = gen(noise)
+        z = torch.logit(m_noise.sample(noise_dim))
         for it in range(t_step):
+            drift = gen(z)
             out = reg * torch.sigmoid(err_ent(z))
-            z = z + out * m.sample(z.shape) * np.sqrt(dt)
+            z = z + drift * dt + out * m.sample(z.shape) * np.sqrt(dt)
             l = l - reg * out.pow(2).mean() * dt
-        l = l + (joint - torch.tile(z, dims=[1, d, 1, 1])).pow(2).mean()
+        z = torch.sigmoid(z)
+        l = l + (joint_raw - torch.tile(z, dims=[1, d, 1, 1])).pow(2).mean()
         opt.zero_grad()
         l.backward()
         opt.step()
